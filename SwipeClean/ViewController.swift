c9ab220dd5c3bfa07c947e4f94b6a470165059c9
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2024-12-30 07:38:33 +0300

    Initial Commit

---
+++ SwipeClean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeClean
+//
+//  Created by Memo on 2024-12-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
